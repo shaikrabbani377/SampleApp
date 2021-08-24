bc750250b55470c5a706036569f8b9a809988977
Rabbu123 <shaikrabbani377@gmail.com>
2021-08-24 17:20:24 +0530

    Employee class moved to Employee.cs from program.cs

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+using System;
+class Employee
+    {
+        public int Id{get; set;}
+        public string Name{get; set;}
+        public double Salary{get;set;}
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Salary}";
+        }
+    }
