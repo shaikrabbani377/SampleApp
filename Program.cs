18a2fa276ce4bb435ee2526485ad985c9f176acb
Rabbu123 <shaikrabbani377@gmail.com>
2021-08-24 16:55:08 +0530

    Employee class defined

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace SampleApp
 {
+    class Employee
+    {
+        public int Id{get; set;}
+        public string Name{get; set;}
+        public double Salary{get;set;}
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Salary}";
+        }
+    } 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Updated!");
+            Employee employee = new Employee{Id=101,Name="Rabbu",Salary=96000};
+            Console.WriteLine(employee);
         }
     }
 }
